359f1ad253dfa14a4625303e036e943afe33ffe3
petrashko <petrashko@tpu.ru>
2021-11-23 14:01:40 +0700

    First Commit

---
+++ geek-app/src/components/message/Message.js
@@ -0,0 +1,12 @@
+//import './message.css';
+
+const Message = ({msg}) => {
+    //
+    return (
+        <div className="message">
+            <p>{msg}</p>
+        </div>
+    );
+}
+
+export default Message;
