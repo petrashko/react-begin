cb08391054c044426bff603ff47b6ca95f97797e
petrashko <petrashko@tpu.ru>
2021-11-23 14:17:51 +0700

    Lesson01

---
+++ lesson01/geek-app/src/components/app/App.js
@@ -0,0 +1,21 @@
+// /* eslint-disable */
+
+import Message from '../message/Message';
+//
+import './App.css';
+
+// eslint-disable-next-line
+const App = () => {
+    //
+    return (
+        <div className="App">
+            <header className="App-header">
+                <Message
+                    msg="Learning React"
+                />
+            </header>
+        </div>
+    );
+}
+
+export default App;
